5c496d5f76e8a9a75413f2e70eb4a5ff2b3021b7
Anthony <anthonymenardgill@gmail.com>
2023-01-12 23:20:29 -0500

    new participants get sent to already connected users. Need to add newusers receiving list of names already there. Need to add removing namese on disconnect

--- movie-voter/src/components/Room.js
+++ movie-voter/src/components/Room.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
-//import { useNavigate } from "react-router-dom";
 const io = require("socket.io-client");
 
-const socket = io("http://localhost:5000");
 const Room = () => {
   const { state } = useLocation();
-  const [participants, setParticipants] = useState([]); // const navigate = useNavigate();
+  const participants = useRef([]);
 
   useEffect(() => {
+    const socket = io("http://localhost:5000");
     socket.on("connect", () => {
       console.log("CONNECTED: ", socket.id, "\n", JSON.stringify({ ...state }));
       socket.emit("join", state.roomID, state.nickname);
@@ -19,13 +18,19 @@ const Room = () => {
       console.log("DISCONNECTED: ", socket.id);
     });
 
-    socket.on("newUser", (userNickname) => {
-      console.log(userNickname, "joined the room.");
-      setParticipants([...participants, userNickname]);
+    socket.on("newUser", (nickname) => {
+      console.log(nickname, "has joined the room.");
+      participants.current = [...participants.current, nickname];
       console.log(participants);
     });
 
-    return () => socket.disconnect();
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("pong");
+      socket.disconnect();
+    };
+    // eslint-disable-next-line
   }, []);
 
   return (
